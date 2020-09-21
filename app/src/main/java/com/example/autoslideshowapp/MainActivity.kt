e4007923c99266631b319bee75bf84ca2ab8a4ad
nagyuu8 <7yuntxcdl@gmail.com>
2020-09-21 22:13:31 +0900

    自動送り機能の追加.Android6.0以下への対応追加

--- app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -15,7 +15,8 @@ import java.util.*
 import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
-    private val mTimer = Timer()
+    private var mTimer: Timer? = null
+    private var mHandler = Handler()
     private val PERMISSIONS_REQUEST_CODE = 100
     private var resumuOnOff = false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,27 +77,45 @@ class MainActivity : AppCompatActivity() {
                             resumuOnOff = false
                             move_button.isClickable = true
                             return_button.isClickable = true
+                            if(mTimer != null){
+                                mTimer!!.cancel()
+                                mTimer = null
+                            }
                         }else{
                             resume_button.text = "停止"
                             resumuOnOff = true
                             move_button.isClickable = false
                             return_button.isClickable = false
+                            if(mTimer == null){
                             //機能2 2秒ごとにスライドを送る。
-                            mTimer.schedule(object :TimerTask(){
-                                if (cursor.isLast) {
-                                    cursor.moveToFirst()
-                                    val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                                    val id=cursor.getLong(fieldIndex)
-                                    val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                                    imageView.setImageURI(imageUri)
-                                } else {
-                                    cursor.moveToNext()
-                                    val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                                    val id=cursor.getLong(fieldIndex)
-                                    val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-                                    imageView.setImageURI(imageUri)
+                            mTimer = Timer()
+                            mTimer!!.schedule(object :TimerTask(){
+                                override fun run() {
+
+                                    if (cursor!!.isLast) {
+                                        cursor!!.moveToFirst()
+                                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                        val id=cursor.getLong(fieldIndex)
+                                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                        mHandler.post{
+                                            imageView.setImageURI(imageUri)
+                                        }
+
+                                    } else {
+                                        cursor!!.moveToNext()
+                                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                        val id=cursor.getLong(fieldIndex)
+                                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                        mHandler.post{
+                                            imageView.setImageURI(imageUri)
+                                        }
+
+                                    }
                                 }
+
                             },0,2000)
+                        }
+
                         }
                     }
                 }
@@ -106,8 +125,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         } else { // Android 5系以下の場合*******************************************************//todo 後でコピーする
-            val resolver = contentResolver
-            val cursor = resolver.query(
+            val resolver=contentResolver
+            val cursor=resolver.query(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                     null, // 項目(null = 全項目)
                     null, // フィルタ条件(null = フィルタなし)
@@ -120,9 +139,85 @@ class MainActivity : AppCompatActivity() {
                 val id=cursor.getLong(fieldIndex)
                 val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
                 imageView.setImageURI(imageUri)
-            }
-        }  // Android 5系以下の場合*******************************************************
+                move_button.setOnClickListener {
+                    if (cursor.isLast) {
+                        cursor.moveToFirst()
+                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        val id=cursor.getLong(fieldIndex)
+                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        imageView.setImageURI(imageUri)
+                    } else {
+                        cursor.moveToNext()
+                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        val id=cursor.getLong(fieldIndex)
+                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        imageView.setImageURI(imageUri)
+                    }
+                }
+                return_button.setOnClickListener {
+                    if (cursor.isFirst) {
+                        cursor.moveToLast()
+                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        val id=cursor.getLong(fieldIndex)
+                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        imageView.setImageURI(imageUri)
+                    } else {
+                        cursor.moveToPrevious()
+                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                        val id=cursor.getLong(fieldIndex)
+                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                        imageView.setImageURI(imageUri)
+                    }
+                }
+                resume_button.setOnClickListener {
+                    if (resumuOnOff) {
+                        resume_button.text="再生"
+                        resumuOnOff=false
+                        move_button.isClickable=true
+                        return_button.isClickable=true
+                        if (mTimer != null) {
+                            mTimer!!.cancel()
+                            mTimer=null
+                        }
+                    } else {
+                        resume_button.text="停止"
+                        resumuOnOff=true
+                        move_button.isClickable=false
+                        return_button.isClickable=false
+                        if (mTimer == null) {
+                            //機能2 2秒ごとにスライドを送る。
+                            mTimer=Timer()
+                            mTimer!!.schedule(object : TimerTask() {
+                                override fun run() {
 
+                                    if (cursor!!.isLast) {
+                                        cursor!!.moveToFirst()
+                                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                        val id=cursor.getLong(fieldIndex)
+                                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                        mHandler.post {
+                                            imageView.setImageURI(imageUri)
+                                        }
+
+                                    } else {
+                                        cursor!!.moveToNext()
+                                        val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                        val id=cursor.getLong(fieldIndex)
+                                        val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                        mHandler.post {
+                                            imageView.setImageURI(imageUri)
+                                        }
+
+                                    }
+                                }
+
+                            }, 0, 2000)
+                        }
+
+                    }
+                }
+            }
+        }// Android 5系以下の場合*******************************************************
     }
 
     private fun getContentsInfo() {
@@ -142,20 +237,7 @@ class MainActivity : AppCompatActivity() {
             val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
             imageView.setImageURI(imageUri)
         }
-//        else if(cursor!!.moveToLast()){
-//            // indexからIDを取得し、そのIDから画像のURIを取得する
-//            cursor.moveToFirst()
-//            val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
-//            val id=cursor.getLong(fieldIndex)
-//            val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-//            imageView.setImageURI(imageUri)
-//        }else{
-//            cursor.moveToNext()
-//            val fieldIndex=cursor.getColumnIndex(MediaStore.Images.Media._ID)
-//            val id=cursor.getLong(fieldIndex)
-//            val imageUri=ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-//            imageView.setImageURI(imageUri)
-//        }
+
         cursor.close()
 
 
